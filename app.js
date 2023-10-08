4511d210aecec22c55cf5a340505b83962904e35
Fadza <fadza0077@gmail.com>
2023-10-08 21:52:16 +0700

    feat: add assets, binding, computed

--- app.js
+++ app.js
@@ -1,65 +1,77 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            loginPageUrl: 'login.html',
-            isShowing: true,
-            showMovieText: 'Hide Movie',
-            mouseOverText: 'Mouse Over',
-            mouseOffsetX: 0,
-            mouseOffsetY: 0,
-            movies: [
-                {
-                    title: 'Avengers Infinity Wars',
-                    description: 'Prequel from Avengers Endgame created by Marvel Cinematic Universe.'
-                },
-                {
-                    title: 'Avengers Endgame',
-                    description: 'Last movie from Avengers Series created by Marvel Cinematic Universe.'
-                },
-                {
-                    title: 'Avengers',
-                    description: 'First movie from Avengers Series created by Marvel Cinematic Universe.'
-                },
-            ]
-        }
+  data() {
+    return {
+      loginPageUrl: "login.html",
+      isShowing: true,
+      showMovieText: "Hide Movie",
+      mouseOverText: "Mouse Over",
+      mouseOffsetX: 0,
+      mouseOffsetY: 0,
+      movies: [
+        {
+          img: "assets/avengers-infinity-war.jpeg",
+          title: "Avengers Infinity Wars",
+          description:
+            "Prequel from Avengers Endgame created by Marvel Cinematic Universe.",
+          isFavorite: true,
+        },
+        {
+          img: "assets/avengers-endgame.jpg",
+          title: "Avengers Endgame",
+          description:
+            "Last movie from Avengers Series created by Marvel Cinematic Universe.",
+          isFavorite: false,
+        },
+        {
+          img: "assets/avengers.jpg",
+          title: "Avengers",
+          description:
+            "First movie from Avengers Series created by Marvel Cinematic Universe.",
+          isFavorite: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    changeTitle() {
+      this.title = "Avengers Endgame";
     },
-    methods: {
-        changeTitle() {
-            this.title = 'Avengers Endgame'
-        },
-        changeDescription(title, studio) {
-            this.description = `Last movie from ${title} Series created by ${studio}. This movie is the last one from the first phase of Marvel Cinematic Universe.`
-        },
-        showMovie() {
-            this.isShowing = !this.isShowing
-            this.showMovieText = this.isShowing ? 'Hide Movie' : 'Show Movie'
-        },
-
-        mouseOver() {
-            this.mouseOverText = 'Cursor in The Box'
-        },
-        mouseLeave() {
-            this.mouseOverText = 'Mouse Over'
-        },
-        doubleClick() {
-            this.mouseOverText = 'Double Click'
-            this.mouseOffsetX = 0;
-            this.mouseOffsetY = 0;
-        },
-        mouseMove(event) { 
-            this.mouseOffsetX = event.offsetX
-            this.mouseOffsetY = event.offsetY
-        }
+    changeDescription(title, studio) {
+      this.description = `Last movie from ${title} Series created by ${studio}. This movie is the last one from the first phase of Marvel Cinematic Universe.`;
+    },
+    showMovie() {
+      this.isShowing = !this.isShowing;
+      this.showMovieText = this.isShowing ? "Hide Movie" : "Show Movie";
     },
 
-    computed: {
-        showTitle() {
-            return this.title.toUpperCase()
-        }
-    }
-})
+    mouseOver() {
+      this.mouseOverText = "Cursor in The Box";
+    },
+    mouseLeave() {
+      this.mouseOverText = "Mouse Over";
+    },
+    doubleClick() {
+      this.mouseOverText = "Double Click";
+      this.mouseOffsetX = 0;
+      this.mouseOffsetY = 0;
+    },
+    mouseMove(event) {
+      this.mouseOffsetX = event.offsetX;
+      this.mouseOffsetY = event.offsetY;
+    },
+    toggleFavorite(movie) {
+      movie.isFavorite = !movie.isFavorite;
+    },
+  },
 
-app.mount('#app')
+  computed: {
+    favoriteMovies() {
+      return this.movies.filter((movie) => movie.isFavorite);
+    },
+    totalFavoriteMovies() {
+      return this.favoriteMovies.length;
+    },
+  },
+});
 
-   
-
+app.mount("#app");
